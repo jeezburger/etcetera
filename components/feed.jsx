7fe929818a26c98842e799382ebca06d61e7afbf
veer aryan <43904888+jeezburger@users.noreply.github.com>
2024-06-14 16:57:47 +0530

    Update feed.jsx

--- components/feed.jsx
+++ components/feed.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Feed = () => {
     return (
-        <div className="flex items-center justify-center h-screen" style={{ backgroundColor: 'black' }}>
+        <div className="flex items-center justify-center h-screen pb-24" style={{ backgroundColor: 'black' }}>
 
             <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
                 Et Cetera
